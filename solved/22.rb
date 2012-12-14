ea9d421d7911f07b24caefa5d9467ca1d0d8b49c
Nicolas McCurdy <thenickperson@gmail.com>
2012-12-14 00:00:09 -0500

    Refactor 22 to make a letter_score util method out of it

--- solved/22.rb
+++ solved/22.rb
@@ -1,9 +1,15 @@
-def score(name, index)
-	sum = 0
-	name.downcase.each_char do |c|
-		sum += c.ord - 96 #index of 'a' in ascii - 1, so our indexes start at 0
+class String
+	def letter_score
+		sum = 0
+		downcase.each_char do |c|
+			sum += c.ord - 96 #index of 'a' in ascii - 1, so our indexes start at 0
+		end
+		sum
 	end
-	sum*(index+1)
+end
+
+def score(name, index)
+	name.letter_score*(index+1)
 end
 
 # Get an array of all the names in the file and sort it
