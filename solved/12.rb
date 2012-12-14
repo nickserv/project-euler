33628aa2837d5fd697a62af02738e365c63c700f
Nicolas McCurdy <thenickperson@gmail.com>
2012-12-13 23:48:56 -0500

    Solve 12

---
+++ solved/12.rb
@@ -0,0 +1,29 @@
+require 'magician'
+
+# Borrowed from kristenmills/Project-Euler
+class Integer
+	def num_divisors primes
+		count = 1
+		remaining = self
+		primes.each do |prime|
+			divisions = 1
+			while remaining % prime == 0
+				divisions += 1
+				remaining /= prime
+			end
+			count *= divisions
+			break if remaining == 1
+		end
+		count
+	end
+end
+
+def triangle n
+	n*(n+1)/2
+end
+
+primes = Math.primes 10_000
+i = 1
+
+i += 1 until triangle(i).num_divisors(primes) > 500
+puts triangle i
